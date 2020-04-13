80164b7200a24a4ceb3c30a84062ab9a1d5d553c
Federico Jimenez <fedeejimenez@gmail.com>
2020-04-13 16:11:46 +0100

    Add styling for upvoting / downvoting

--- src/Joke.js
+++ src/Joke.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowUp, faArrowDown } from "@fortawesome/free-solid-svg-icons";
+import "./Joke.css";
 
 class Joke extends React.Component {
   constructor() {
@@ -14,11 +15,26 @@ class Joke extends React.Component {
     return (
       <div className="Joke">
         <div className="Joke-buttons">
-          <FontAwesomeIcon icon={faArrowUp} onClick={this.props.upvote} />
-          <span>{this.props.votes}</span>
-          <FontAwesomeIcon icon={faArrowDown} onClick={this.props.downvote} />
+          <FontAwesomeIcon
+            className="fa-arrow"
+            icon={faArrowUp}
+            onClick={this.props.upvote}
+          />
+          <span className="Joke-votes">{this.props.votes}</span>
+          <FontAwesomeIcon
+            className="fa-arrow"
+            icon={faArrowDown}
+            onClick={this.props.downvote}
+          />
         </div>
         <div className="Joke-text">{this.props.text}</div>
+        <div className="Joke-smiley">
+          <i
+            class="em em-rolling_on_the_floor_laughing"
+            aria-role="presentation"
+            aria-label="ROLLING ON THE FLOOR LAUGHING"
+          ></i>
+        </div>
       </div>
     );
   }
