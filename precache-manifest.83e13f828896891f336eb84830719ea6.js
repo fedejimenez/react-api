d7076d8c452bff803b40909fe665d7958eb51cdb
Federico Jimenez <fedeejimenez@gmail.com>
2020-04-13 18:36:37 +0100

    Updates

---
+++ precache-manifest.83e13f828896891f336eb84830719ea6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b99cfa0c940985b9388a97270125513",
+    "url": "/react-jokes/index.html"
+  },
+  {
+    "revision": "efa44d4f08e9597892da",
+    "url": "/react-jokes/static/css/main.84fce7a5.chunk.css"
+  },
+  {
+    "revision": "da2ad7094c7023640a31",
+    "url": "/react-jokes/static/js/2.175cc416.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-jokes/static/js/2.175cc416.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efa44d4f08e9597892da",
+    "url": "/react-jokes/static/js/main.85755e54.chunk.js"
+  },
+  {
+    "revision": "431191fa737972920af1",
+    "url": "/react-jokes/static/js/runtime-main.fee20fbf.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-api/precache-manifest.a57b3041097465b1570dff92e81043bf.js"
+  "/react-jokes/precache-manifest.83e13f828896891f336eb84830719ea6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-api/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-jokes/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
