ecb07fc29607dc31c89d2a8317a9636b7643b19a
Federico Jimenez <fedeejimenez@gmail.com>
2020-04-13 14:25:56 +0100

    Updates

---
+++ precache-manifest.4070e25ed7052254083529d24d3257a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7055373e99eea2799f0c0bc04b8f46e4",
+    "url": "/react-api/index.html"
+  },
+  {
+    "revision": "dd862f5cc768c88e6e60",
+    "url": "/react-api/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "57e0fa397421a990a1b8",
+    "url": "/react-api/static/js/2.407356c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-api/static/js/2.407356c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd862f5cc768c88e6e60",
+    "url": "/react-api/static/js/main.3efeabfa.chunk.js"
+  },
+  {
+    "revision": "ba28837e8aea51f6eb2b",
+    "url": "/react-api/static/js/runtime-main.5d1aa0af.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-api/static/media/logo.5d5d9eef.svg"
+  }
+]);
